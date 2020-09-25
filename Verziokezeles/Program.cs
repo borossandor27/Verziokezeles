c627ba33b05cae497448ceb5648920e2e171c394
borossandor27 <boros.sandor@petrik.hu>
2020-09-25 19:04:41 +0200

    A számokat 20-ig írja ki

--- Verziokezeles/Program.cs
+++ Verziokezeles/Program.cs
@@ -10,7 +10,7 @@ namespace Verziokezeles
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 20; i++)
             {
                 Console.WriteLine(i);
             }
